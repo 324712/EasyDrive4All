3584e5ea4931dd8f5c9c99596f098785ce8723fd
Mitchel Jansen <324712@student.mboutrecht.nl>
2022-06-21 10:32:39 +0200

    Get Lessons from database

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lessons;
 use Illuminate\Http\Request;
 
 class LessonsController extends Controller
 {
     public function index()
     {
-        return view('lessons.index');
+        /*
+         * De data ophalen uit de database met de relatie in de lesson_type table dat we uit de model Lessons ophalen.
+         * Als er veel data aanwezig is dient deze niet direct opgehaald te worden, maar pas gecollect te worden als hier
+         * om gevraagd word.
+         */
+        $lessons = Lessons::with('lessonType')->orderBy('created_at', 'desc')->lazy()->collect();
+
+        /*
+         * Return de view naar de bezoeker met de opgehaalde data uit $lessons.
+         */
+        return view('lessons.index')->with([
+            'lessons'   =>  $lessons,
+        ]);
     }
 }
