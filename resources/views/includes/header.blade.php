f9ee51219427f08f4778c90a12b537bfcf3e4cf5
Mitchel Jansen <324712@student.mboutrecht.nl>
2022-06-21 14:16:49 +0200

    Header front-end

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+    <div class="px-3 py-2 bg-dark text-white mb-5">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+                    Autorijschool Easy Drive 4 All
+                </a>
+
+                <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                    <li>
+                        <a href="{{ route('lessons.index') }}" class="nav-link {{ checkActive('lessons.index') }}">
+                            <i class="far fa-car fa-fw bi d-block mx-auto mb-1"></i>
+                            Overzicht Rijlessen
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('lessons.archive') }}" class="nav-link {{ checkActive('lessons.archive') }}">
+                            <i class="far fa-archive fa-fw bi d-block mx-auto mb-1"></i>
+                            Archief Rijlessen
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('lessons.create') }}" class="nav-link {{ checkActive('lessons.create') }}">
+                            <i class="far fa-calendar-plus fa-fw bi d-block mx-auto mb-1"></i>
+                            Rijles inplannen
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
