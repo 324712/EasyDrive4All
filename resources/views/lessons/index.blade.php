15a379519d3107f2b9b2e675ba426c1c7d492bac
Mitchel Jansen <324712@student.mboutrecht.nl>
2022-06-21 10:33:16 +0200

    Store data in front-end

--- resources/views/lessons/index.blade.php
+++ resources/views/lessons/index.blade.php
@@ -2,5 +2,18 @@
 @section('pagename', 'Lessons')
 
 @section('content')
-    lessons
+    @forelse($lessons as $lesson)
+        <p><strong>Lestype:</strong> {{ $lesson->lessonType->name }}</p>
+        <p><strong>Lestype info:</strong> {{ $lesson->lessonType->description }}</p>
+        <p><strong>Instructeur:</strong> {{ $lesson->instructor->fullname }}</p>
+        <p><strong>Student:</strong> {{ $lesson->student->fullname }}</p>
+        <p><strong>Resterende lesuren:</strong> {{ $lesson->student->lessons }}</p>
+        <p><strong>Ophaallocatie:</strong> {{ $lesson->location }}</p>
+        <p><strong>Datum:</strong> {{ $lesson->date }}</p>
+        <p><strong>Begintijd:</strong> {{ $lesson->begin_time }}</p>
+        <p><strong>Eindtijd:</strong> {{ $lesson->end_time }}</p>
+        <p><strong>Informatie:</strong> {{ $lesson->information }}</p>
+    @empty
+        Er zijn momenteel geen ingeplande lessen.
+    @endforelse
 @endsection
