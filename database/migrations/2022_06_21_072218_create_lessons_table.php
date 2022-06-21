ecbda5607394b901ffb521544521643b17db161e
Mitchel Jansen <324712@student.mboutrecht.nl>
2022-06-21 10:33:01 +0200

    Create migrations

--- database/migrations/2022_06_21_072218_create_lessons_table.php
+++ database/migrations/2022_06_21_072218_create_lessons_table.php
@@ -13,13 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lessons', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('lesson_type');
+            $table->foreignId('student_id');
+            $table->foreignId('instructor_id');
+            $table->string('location');
+            $table->date('date');
+            $table->time('begin_time');
+            $table->time('end_time');
+            $table->mediumText('information');
             $table->timestamps();
         });
     }
@@ -31,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lessons');
     }
 };
