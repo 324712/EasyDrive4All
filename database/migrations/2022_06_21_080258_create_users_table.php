8317d0678b649f96c78d34b567b27f3a8ae17ffd
Mitchel Jansen <324712@student.mboutrecht.nl>
2022-06-22 10:29:31 +0200

    Update 2022_06_21_080258_create_users_table.php

--- database/migrations/2022_06_21_080258_create_users_table.php
+++ database/migrations/2022_06_21_080258_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('fullname');
+            $table->string('location');
             $table->integer('lessons');
             $table->timestamps();
         });
